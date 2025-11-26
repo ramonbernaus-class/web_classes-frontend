e70e678efc7a2f93e2d03a19b4e968e5b0f737f5
Ramon <ramon.bernaus.torrents@gmail.com>
2025-11-26 15:37:04 +0100

    Fix CSS

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -84,10 +84,6 @@ const registrarse = async () => {
     </form>
     <button @click="registrarse" class="btn-register">Registrarse</button>
   </div>
-  <div class="text-blue-500 text-4xl font-bold">
-  Test Tailwind
-</div>
-
 </template>
 
 <style scoped>
